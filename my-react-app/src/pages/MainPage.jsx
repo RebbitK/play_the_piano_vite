593c12aeca2bc400b3505400a1447b1e92aaf50d
RebbitK <andxl9979@gmail.com>
2024-12-15 16:49:29 +0900

    :recycle: 테스트 화면 비율 재조정

--- my-react-app/src/pages/MainPage.jsx
+++ my-react-app/src/pages/MainPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import BasicLayout from "../layouts/BasicLayout";
+
 const MainPage = () => {
   return (
       <BasicLayout>
-        <div className="  bg-sky-300">Main Page</div>
+        <div className="flex items-center justify-center bg-sky-300 h-full w-full">
+          <h1 className="text-6xl font-bold text-white">Welcome to the Main Page</h1>
+        </div>
       </BasicLayout>
   );
-}
+};
+
 export default MainPage;
