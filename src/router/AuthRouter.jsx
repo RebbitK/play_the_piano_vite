a15e66ce2d3ec0287bda96c5756a9c63453969f3
RebbitK <andxl9979@gmail.com>
2024-12-23 21:48:32 +0900

    :sparkles: 아이디, 비밀번호 찾기 router 추가

--- src/router/AuthRouter.jsx
+++ src/router/AuthRouter.jsx
@@ -1,8 +1,9 @@
 import { Suspense, lazy } from "react";
 
 const Loading = <div>Loading....</div>;
-const Login = lazy(() => import("../pages/LoginPage.jsx"));
-const Signup = lazy(()=>import ("../pages/SignupPage.jsx"));
+const Login = lazy(() => import("../pages/LoginPage.jsx"))
+const Signup = lazy(()=>import ("../pages/SignupPage.jsx"))
+const Username = lazy(() =>import ("../pages/FindUsernamePage.jsx"))
 
 
 const authRouter = () => {
@@ -19,6 +20,13 @@ const authRouter = () => {
       element: (
           <Suspense fallback={Loading}>
             <Signup />
+          </Suspense>
+      )
+    },{
+    path: "username",
+      element: (
+          <Suspense fallback={Loading}>
+            <Username />
           </Suspense>
       )
     }
