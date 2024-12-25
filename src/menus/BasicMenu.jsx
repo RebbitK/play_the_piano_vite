4571b422daa30cd20f903dae494d32c66372fd84
RebbitK <andxl9979@gmail.com>
2024-12-25 16:45:48 +0900

    :sparkles: 사이트 매인 배너 추가

--- src/menus/BasicMenu.jsx
+++ src/menus/BasicMenu.jsx
@@ -1,40 +1,48 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { jwtDecode } from "jwt-decode";
-import {getCookie} from "../util/CookieUtil.jsx";
+import {useSelector} from "react-redux"
+import {Link} from "react-router-dom"
+import {getCookie} from "../util/CookieUtil.jsx"
+import banner from "../img/Play the piano.png"
 
 const BasicMenu = () => {
-  const loginState = useSelector((state) => state.loginSlice);
-  const jwt = getCookie("accessToken");
-  const isLoggedIn = !!jwt;
-  const isSeller = isLoggedIn && jwtDecode(jwt).role === "SELLER";
-  const isAdmin = isLoggedIn && jwtDecode(jwt).role === "ADMIN";
+  const loginState = useSelector((state) => state.loginSlice)
+  const jwt = getCookie("accessToken")
+  const isLoggedIn = !!jwt
 
   return (
-      <nav className="flex items-center justify-between bg-orange-500 px-6 py-4">
-        <ul className="flex space-x-6 text-white font-bold text-lg">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-        </ul>
-
-        <div>
-          <Link
-              to="/auth/login"
-              className="bg-white text-orange-500 px-4 py-2 rounded font-semibold"
-          >
-            Login
-          </Link>
+      <>
+        <div className="w-full bg-cover bg-center" style={{
+          backgroundImage: `url(${banner})`,
+          height: 'calc(100vw * 1 / 16)',
+        }}>
+          <div
+              className="flex justify-center items-center h-full bg-white bg-opacity-40"></div>
         </div>
-      </nav>
-  );
-};
 
+        <nav
+            className="flex items-center justify-between bg-orange-500 px-6 py-4">
+          <ul className="flex space-x-6 text-white font-bold text-lg">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+          </ul>
 
-export default BasicMenu;
+          <div>
+            <Link
+                to="/auth/login"
+                className="bg-white text-orange-500 px-4 py-2 rounded font-semibold"
+            >
+              Login
+            </Link>
+          </div>
+        </nav>
+      </>
+  )
+}
+
+export default BasicMenu
